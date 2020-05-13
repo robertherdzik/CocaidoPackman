742fdefde12be2b05d34fdfc89dc6088be156024
Robert Herdzik <robert.herdzik@yahoo.com>
2020-05-13 17:11:17 +0200

    Base scaffold

--- CocaidoPackman/CocaidoPackmanTests/CocaidoPackmanTests.swift
+++ CocaidoPackman/CocaidoPackmanTests/CocaidoPackmanTests.swift
@@ -1,34 +1,99 @@
-//
-//  CocaidoPackmanTests.swift
-//  CocaidoPackmanTests
-//
-//  Created by Robert Herdzik on 13/05/2020.
-//  Copyright © 2020 Robert Herdzik. All rights reserved.
-//
-
 import XCTest
 @testable import CocaidoPackman
 
 class CocaidoPackmanTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testMoveHeroToRight() {
+        let hero = Hero(position: .init(x: 0, y: 0))
+        let sut = Game(hero: hero)
+        
+        sut.moveHero(direction: .right)
+        let expectedViewModel = GameViewModel(heroPosition: .init(x: 1, y: 0))
+       
+        XCTAssertEqual(sut.output(), expectedViewModel)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
+
+//------------------------------------------------
+// _TODO [🌶]: implementation
+
+struct Tile {
+    let x, y: Int
+}
+
+struct Board {
+    let area: [Tile]
+}
+
+class Game {
+    private var board: Board!
+    private let boardSize: CGSize = CGSize(width: 10, height: 10)
+    private let hero: Hero
+    private var viewModel: GameViewModel!
+    
+    init(hero: Hero) {
+        self.hero = hero
+        let heroPosition = hero.position
+        viewModel = GameViewModel(heroPosition: .init(x: heroPosition.x, y: heroPosition.y))
+        makeBoard()
+    }
+    
+    func makeBoard() {
+        var tiles = [Tile]()
+        for i in 0..<Int(boardSize.width) {
+            for j in 0..<Int(boardSize.height) {
+                tiles.append(Tile(x: i, y: j))
+            }
+        }
+        board = Board(area: tiles)
+    }
+    
+    func moveHero(direction: Direction) {
+        var coordinate = viewModel.heroPosition
+        switch direction {
+        case .up:
+            break
+        case .down:
+            break
+        case .left:
+            break
+        case .right:
+            coordinate.x += 1
+        }
+        
+        let localViewModel = GameViewModel(heroPosition: coordinate)
+        
+        viewModel = localViewModel
+    }
+    
+    func output() -> GameViewModel {
+        viewModel
+    }
+}
+
+enum Direction {
+    case up, down, left, right
+}
+
+struct Hero {
+    var position: Tile
+}
+
+//------------------------------------------------
+
+struct Coordinate: Equatable {
+    var x, y: Int
+}
+
+struct GameViewModel: Equatable {
+    let heroPosition: Coordinate
+}
+
+/*
+ Board
+ -
+ 
+ Packman
+ - move up/down/right/left
+ - position
+ */
