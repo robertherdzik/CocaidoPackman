a5b84196e8e01006123d037d23b965b8076a0f1d
Robert Herdzik <robert.herdzik@yahoo.com>
2020-05-20 09:06:37 +0200

    Add Mounir code

--- CocaidoPackman/CocaidoPackmanTests/CocaidoPackmanTests.swift
+++ CocaidoPackman/CocaidoPackmanTests/CocaidoPackmanTests.swift
@@ -12,12 +12,93 @@ class CocaidoPackmanTests: XCTestCase {
        
         XCTAssertEqual(sut.output(), expectedViewModel)
     }
+    
+    func testMoveHeroToLeft() {
+        let hero = Hero(position: .init(x: 1, y: 0))
+        let sut = Game(hero: hero)
+        
+        sut.moveHero(direction: .left)
+        let expectedViewModel = GameViewModel(heroPosition: .init(x: 0, y: 0))
+       
+        XCTAssertEqual(sut.output(), expectedViewModel)
+    }
+    
+    func testMoveHeroToUp() {
+        let hero = Hero(position: .init(x: 0, y: 1))
+        let sut = Game(hero: hero)
+        
+        sut.moveHero(direction: .up)
+        let expectedViewModel = GameViewModel(heroPosition: .init(x: 0, y: 0))
+       
+        XCTAssertEqual(sut.output(), expectedViewModel)
+    }
+    
+    func testMoveHeroToDown() {
+        let hero = Hero(position: .init(x: 0, y: 0))
+        let sut = Game(hero: hero)
+        
+        sut.moveHero(direction: .down)
+        let expectedViewModel = GameViewModel(heroPosition: .init(x: 0, y: 1))
+       
+        XCTAssertEqual(sut.output(), expectedViewModel)
+    }
+    
+    func testMoveHeroToUpDoesNotGoOutOfBounds() {
+        let hero = Hero(position: .init(x: 0, y: 0))
+        let sut = Game(hero: hero)
+        
+        sut.moveHero(direction: .up)
+        let expectedViewModel = GameViewModel(heroPosition: .init(x: 0, y: 0))
+       
+        XCTAssertEqual(sut.output(), expectedViewModel)
+    }
+    
+    func testMoveHeroToLeftDoesNotGoOutOfBounds() {
+        let hero = Hero(position: .init(x: 0, y: 0))
+        let sut = Game(hero: hero)
+        
+        sut.moveHero(direction: .left)
+        let expectedViewModel = GameViewModel(heroPosition: .init(x: 0, y: 0))
+       
+        XCTAssertEqual(sut.output(), expectedViewModel)
+    }
+    
+    func testMoveHeroToDownDoesNotGoOutOfBounds() {
+        let hero = Hero(position: .init(x: 9, y: 9))
+        let sut = Game(hero: hero)
+        
+        sut.moveHero(direction: .down)
+        let expectedViewModel = GameViewModel(heroPosition: .init(x: 9, y: 9))
+       
+        XCTAssertEqual(sut.output(), expectedViewModel)
+    }
+    
+    func testMoveHeroToRightDoesNotGoOutOfBounds() {
+        let hero = Hero(position: .init(x: 9, y: 9))
+        let sut = Game(hero: hero)
+        
+        sut.moveHero(direction: .right)
+        let expectedViewModel = GameViewModel(heroPosition: .init(x: 9, y: 9))
+       
+        XCTAssertEqual(sut.output(), expectedViewModel)
+    }
+    
+    func testMoveHeroToLeftDoesNotGoThroughWall() {
+        let hero = Hero(position: .init(x: 2, y: 2))
+        let sut = Game(hero: hero)
+        
+        sut.moveHero(direction: .left)
+        let expectedViewModel = GameViewModel(heroPosition: .init(x: 2, y: 2))
+       
+        XCTAssertEqual(sut.output(), expectedViewModel)
+    }
 }
 
 //------------------------------------------------
 // _TODO [🌶]: implementation
 
 struct Tile {
+    var isWallTile = false
     let x, y: Int
 }
 
@@ -25,11 +106,16 @@ struct Board {
     let area: [Tile]
 }
 
+struct BoardSize {
+    let width: Int
+    let height: Int
+}
+
 class Game {
     private var board: Board!
-    private let boardSize: CGSize = CGSize(width: 10, height: 10)
+    private let boardSize: BoardSize = BoardSize(width: 10, height: 10)
     private let hero: Hero
-    private var viewModel: GameViewModel!
+    private var viewModel: GameViewModel
     
     init(hero: Hero) {
         self.hero = hero
@@ -38,31 +124,56 @@ class Game {
         makeBoard()
     }
     
-    func makeBoard() {
+    private func makeBoard() {
         var tiles = [Tile]()
-        for i in 0..<Int(boardSize.width) {
-            for j in 0..<Int(boardSize.height) {
-                tiles.append(Tile(x: i, y: j))
+        
+        let wallCoordinates = [Coordinate(x: 1, y: 1),
+                               Coordinate(x: 1, y: 2),
+                               Coordinate(x: 1, y: 3)]
+        
+        for i in 0..<boardSize.width {
+            for j in 0..<boardSize.height {
+                var tile = Tile(x: i, y: j)
+                let currentCoordinate = Coordinate(x: i, y: j)
+                if wallCoordinates.contains(currentCoordinate) {
+                    tile.isWallTile = true
+                }
+                tiles.append(tile)
             }
         }
         board = Board(area: tiles)
     }
     
-    func moveHero(direction: Direction) {
-        var coordinate = viewModel.heroPosition
+    private func moveTo(_ direction: Direction) -> Coordinate {
+        let coordinate = viewModel.heroPosition
+        
         switch direction {
         case .up:
-            break
+            return coordinate.moveUp()
         case .down:
-            break
+            return coordinate.moveDown(max: boardSize.height)
         case .left:
-            break
+            let tileLeftOfPos = board.area
+                .filter({ tile -> Bool in
+                    if tile.x == coordinate.x - 1 && tile.y == coordinate.y {
+                        return true
+                    }
+                    return false
+                })
+                .first
+            
+            if let tileLeftOfPos = tileLeftOfPos {
+               return coordinate.moveLeft(from: tileLeftOfPos)
+            }
+            return coordinate
         case .right:
-            coordinate.x += 1
+            return coordinate.moveRight(max: boardSize.width)
         }
-        
+    }
+    
+    func moveHero(direction: Direction) {
+        let coordinate = moveTo(direction)
         let localViewModel = GameViewModel(heroPosition: coordinate)
-        
         viewModel = localViewModel
     }
     
@@ -83,6 +194,34 @@ struct Hero {
 
 struct Coordinate: Equatable {
     var x, y: Int
+    
+    func moveUp() -> Coordinate {
+        if y != 0 {
+            return Coordinate(x: x, y: y - 1)
+        }
+        return self
+    }
+    
+    func moveDown(max: Int) -> Coordinate {
+        if y != max - 1 {
+            return Coordinate(x: x, y: y + 1)
+        }
+        return self
+    }
+    
+    func moveLeft(from tileLeftOfPos: Tile) -> Coordinate {
+        if x != 0 && !tileLeftOfPos.isWallTile {
+            return Coordinate(x: x - 1, y: y)
+        }
+        return self
+    }
+    
+    func moveRight(max: Int) -> Coordinate {
+        if x != max - 1 {
+            return Coordinate(x: x + 1, y: y)
+        }
+        return self
+    }
 }
 
 struct GameViewModel: Equatable {
